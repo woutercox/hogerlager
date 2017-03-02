1699e802c1d5754e92a435e8079675e86ae9e43e
User112 <User112>
2017-03-02 15:55:26 +0100

    Werkende versie

--- javascript/functies.js
+++ javascript/functies.js
@@ -1,12 +1,31 @@
 window.onload = init;
 
+var rand = Math.floor(Math.random() * 100 +1);
 function init(){
-    console.log('test');
-    document.getElementById('hoger').addEventListener('click', nieuwGetal);
-    document.getElementById('lager').addEventListener('click', nieuwGetal);
+    document.getElementById('gok').addEventListener('click', nieuwGetal);
 }
 
 function nieuwGetal(event){
     event.preventDefault();
-   
+    var input = document.getElementById('getal').value
+    var output = document.getElementById('display');
+    if (input > rand)
+        {
+            output.innerHTML = 'Lager';
+        }
+    else if (input < rand)
+        {
+            output.innerHTML = 'Hoger';
+        }
+    else if (input == rand)
+        {
+            output.innerHTML = 'Hoera gewonnen! Het getal was '+input;
+        }
+    else if (isNaN(input))
+        {
+            output.innerHTML = 'Geef een getal in foefelaar!';
+        }
+    else{
+        output.innerHTML = "'T is kapot :'(";
+    }
 }
